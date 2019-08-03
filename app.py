bfe71026c1b23e2ef57e39250e2ed1ee412f2de0
Kiraa Corsac <wire150@gmail.com>
2019-08-03 16:12:02 +0200

    actually call

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 def hello():
     homes = scrape_bezrealitky.request()[0]
     if len(homes) > 0:
-        #call_me.call_people_to_alert()
+        call_me.call_people_to_alert()
         return json.dumps(homes)
     return "nothing new" + "<br>" + ("-"*50) + "<br>" + json.dumps(alerted.get_called_list())
 
--- scrape_bezrealitky.py
+++ scrape_bezrealitky.py
@@ -1,6 +1,5 @@
 import requests
 import json
-import call_me
 import alerted
 import os
 
