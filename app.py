677ad702a70c526585321b692d80a9b4a6116818
Kiraa Corsac <wire150@gmail.com>
2019-09-10 20:04:38 +0200

    remove bullshit

--- app.py
+++ app.py
@@ -13,14 +13,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    try:
-        molbio = spam_attempt.request()
-        homes = scrape_bezrealitky.request()[0]
-        if len(homes) > 0:
-        #     call_me.call_people_to_alert()
-             return json.dumps(homes)
-        return str("Autozápis proveden" in "lol") + "<br>" + "nothing new" + "<br>" + ("-"*50) + "<br>" + json.dumps(alerted.get_called_list())
-    except Exception as exc:
-        return "<br>".join(traceback.format_exception(type(exc), exc, exc.__traceback__))
+    molbio = spam_attempt.request()
+    homes = scrape_bezrealitky.request()[0]
+    if len(homes) > 0:
+    #     call_me.call_people_to_alert()
+            return json.dumps(homes)
+    return str("Autozápis proveden" in molbio.content) + "<br>" + "nothing new" + "<br>" + ("-"*50) + "<br>" + json.dumps(alerted.get_called_list())
 if __name__ == "__main__":
     app.run(port=80, debug=True)
