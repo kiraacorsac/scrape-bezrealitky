697b3f932b9255da881411dd3aada0d0d3811663
Kiraa Corsac <wire150@gmail.com>
2019-10-04 16:50:48 +0200

    rollback

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask
 import scrape_bezrealitky
-import spam_attempt
-#import call_me
+import call_me
 import alerted
 import json
 import traceback
@@ -13,11 +12,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    molbio = spam_attempt.request()
     homes = scrape_bezrealitky.request()[0]
     if len(homes) > 0:
-    #     call_me.call_people_to_alert()
-            return json.dumps(homes)
-    return str("Autozápis proveden" in molbio.content) + "<br>" + "nothing new" + "<br>" + ("-"*50) + "<br>" + json.dumps(alerted.get_called_list())
+        call_me.call_people_to_alert()
+        return json.dumps(homes)
+    return "<br>" + "nothing new" + "<br>" + ("-"*50) + "<br>" + json.dumps(alerted.get_called_list())
 if __name__ == "__main__":
     app.run(port=80, debug=True)
